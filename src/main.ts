c1ae3505a6af88307f7b4cffe64cd8e027b260cb
felips2ribeiro <felips2ribeiro@gmail.com>
2025-05-11 13:06:47 -0300

    Configuração do Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Configurar Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Photovoltaic Monitoring API')
+    .setDescription('API for monitoring photovoltaic power plants')
+    .setVersion('1.0')
+    .addTag('plants', 'Operations related to power plants')
+    .addTag('inverters', 'Operations related to inverters')
+    .addTag('metrics', 'Operations related to metrics data ingestion')
+    .addTag('analytics', 'Operational insights and aggregations')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  const port = configService.get<number>('API_PORT') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(
+    `Swagger documentation is available at: ${await app.getUrl()}/api-docs`,
+  );
 }
-bootstrap();
+
+void bootstrap();
